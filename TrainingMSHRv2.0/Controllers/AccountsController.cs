a2e6d01b281c1d15d9ad0c58e55e299852356ae0
Naufal Aji Wibowo <naufal.aji@mii.co.id>
2020-11-26 15:50:22 +0700

    initial

---
+++ TrainingMSHRv2.0/Controllers/AccountsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrainingMSHRv2._0.Controllers
+{
+    public class AccountsController : Controller
+    {
+        // GET: Accounts
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ TrainingMSHRv2.0/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrainingMSHRv2._0.MvcApplication" Language="C#" %>
